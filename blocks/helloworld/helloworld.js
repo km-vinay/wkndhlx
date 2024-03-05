2300019d449b6daa666b24d4973725fc65175760
Vinay <vinay.maheswaraia@adobe.com>
2024-03-05 12:41:26 +0530

    using ffetch

--- blocks/helloworld/helloworld.js
+++ blocks/helloworld/helloworld.js
@@ -1,17 +1,14 @@
+import ffetch from '../../scripts/ffetch.js';
+
 async function queryDemo() {
-	const offset = 0;
 	const limit = 20;
-	const api = new URL('https://main--wkndhlx--km-vinay.hlx.live/query-index.json?sheet=default');
-	api.searchParams.append("offset", JSON.stringify(offset));
-	api.searchParams.append("limit", limit);
-	const response = await fetch(api, {});
-	const result = await response.json();
+	const entries = ffetch('/query-index.json').sheet('default').limit(limit);
 	const table = document.createElement('table');
-	result.data.forEach(async (post) => {
+	for await (const post of entries) {
 		const tr = document.createElement('tr');
 		tr.innerHTML = `<td>${post.title}</td><td>${post.description}</td>`;
 		table.append(tr);
-	});
+	}
 	return table;
 }
 
@@ -20,6 +17,8 @@ export default async function decorate(block) {
 	[...block.children].forEach((row) => {
 		const image = row.getElementsByTagName('img')[0];
 		row.innerHTML = `<div><img src = ${image.src}></img><br/><p>${row.innerText}</p></div>`;
+
 	});
 	block.append(table);
+
 }
