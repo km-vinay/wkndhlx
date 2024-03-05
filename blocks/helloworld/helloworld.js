2dadb555f917052b50ef7f2898be13e6a1bb43d7
Vinay <vinay.maheswaraia@adobe.com>
2024-03-05 12:37:41 +0530

    updating helloworld js to fetch and display data from query-index json

--- blocks/helloworld/helloworld.js
+++ blocks/helloworld/helloworld.js
@@ -1,3 +1,20 @@
+async function queryDemo() {
+	const offset = 0;
+	const limit = 20;
+	const api = new URL('https://main--wkndhlx--km-vinay.hlx.live/query-index.json?sheet=default');
+	api.searchParams.append("offset", JSON.stringify(offset));
+	api.searchParams.append("limit", limit);
+	const response = await fetch(api, {});
+	const result = await response.json();
+	const table = document.createElement('table');
+	result.data.forEach(async (post) => {
+		const tr = document.createElement('tr');
+		tr.innerHTML = `<td>${post.title}</td><td>${post.description}</td>`;
+		table.append(tr);
+	});
+	return table;
+}
+
 export default async function decorate(block) {
 	const table = await queryDemo();
 	[...block.children].forEach((row) => {
